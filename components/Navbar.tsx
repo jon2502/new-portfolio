d26b1c87f9a4d220933d7e4d21f872c84efc1409
jon2502 <jonbjarke25@outlook.dk>
2025-08-03 14:07:30 +0200

    setup Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='px-8 py-3'>
+        <nav className="flex justify-between items-center">
+            <Link href="/">
+                <Image src="/Logo.png" alt="logo" width={380} height={75} />
+            </Link>
+            <div className='flex items-center gap-5'>
+            <Link href="/startup/create">
+                <span>Multimediedesigner Cases</span>
+            </Link>
+            <Link href="/startup/create">
+                <span>Webudvikling cases</span>
+            </Link>
+            <Link href="/startup/create">
+                <span>Adobe Programmer</span>
+            </Link>
+            <Link href="/startup/create">
+                <span>Kodning</span>
+            </Link>
+            <Link href="/startup/create">
+                <span>Om Mig</span>
+            </Link>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
