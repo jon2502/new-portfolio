0aae6eafb9956f5d64605a269626758aa2ff5d8f
jon2502 <jonbjarke25@outlook.dk>
2025-08-06 10:19:31 +0200

    setup of how data will be managed (subject to change)

--- app/(root)/Adobe_Programmer/[id]/page.tsx
+++ app/(root)/Adobe_Programmer/[id]/page.tsx
@@ -1,19 +1,34 @@
+import ThreeImages from '@/components/SetupTypes/ThreeImages';
 import { createClient } from '@/utils/supabase/client';
 import React from 'react'
 
 const page = async ({params}: {params: Promise<{ id:string }>}) => {
   const id = (await params).id
+
   const supabase = createClient();
   const { data, error } = await supabase
           .from('Adobe projects')
           .select()
           .eq('Program', id)
+          .order('id', { ascending: true})
+
+  const componentSelctor = ((project: any) => {
+    console.log(project)
+    switch(project.SetupType) {
+      case "ThreeImages":
+        return <ThreeImages key={project?.id} project={project}/>
+    }
+  })
+
   return (
     <>
-    <div>
-      
-    </div>
-    <pre>{JSON.stringify(data, null, 2)}</pre>
+    <section>
+      {data?.map((project) => (
+        <div key={project?.id}>
+          {componentSelctor(project)}
+        </div>
+      ))}
+    </section>
     </>
   )
 }
--- components/SetupTypes/ThreeImages.tsx
+++ components/SetupTypes/ThreeImages.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const ThreeImages = () => {
+const ThreeImages = ({project}: {project:any}) => {
+  const {Title, Text, Info} = project
+
+  type Card = {
+  Text: string;
+  Image: String;
+  }
+  
   return (
-    <div>ThreeImages</div>
+    <>
+      <h1>{Title}</h1>
+      <p>{Text}</p>
+      <div>
+        {Info.map((card:Card) =>(
+          <div key={card.Text}>
+            <p>{card.Text}</p>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
