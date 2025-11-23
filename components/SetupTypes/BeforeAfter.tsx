3243d8458b0f452d46a0d35fdf05d27c14c0871a
jon2502 <blasterblade892@gmail.com>
2025-11-23 11:12:49 +0100

    started work on slide feature

--- components/SetupTypes/BeforeAfter.tsx
+++ components/SetupTypes/BeforeAfter.tsx
@@ -14,25 +14,34 @@ const BeforeAfter = ({project}: {project:Project}) => {
         <p>{Text}</p>
         <p>(hvis der ikke er noget prøv at genindlæs siden)</p>
       </div>
-      <div>
+      <div className='flex'>
          {Info.map((card) =>(
-          <div key={card.OriginalIMG}>
-            <Image
-              src={card.OriginalIMG}
-              alt="Original Image"
-              width={500}
-              height={500}
-              className="absolute object-cover"
-            />
-            <Image
-              src={card.NewIMG}
-              alt="New Image"
-              width={500}
-              height={500}
-              className="object-cover"
-            />
-          </div>
+          <section key={card.OriginalIMG} className='mx-[10px] w-[100%] h-[100%]'>
+            <div className='w-[100%] h-[100%]'>
+              <Image
+                src={card.OriginalIMG}
+                alt="Original Image"
+                width={500}
+                height={500}
+                className="absolute object-cover"
+              />
+              <Image
+                src={card.NewIMG}
+                alt="New Image"
+                width={500}
+                height={500}
+                className="object-cover"
+              />
+            </div>
+            <div className='absolute w-[100%] h-[100%] top-0 z-40'>
+              <div className='absolute w-[38px] h-[38px] top-[50%] border-solid border-[3px] border-[#fff] rounded-full'>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </section>
          ))}
+         
       </div>
     </div>
   )
