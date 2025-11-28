99bf992eb752fa06eb761c2ddc9bdbadf7f10e72
jon2502 <blasterblade892@gmail.com>
2025-11-28 15:30:10 +0100

    added more content

--- app/(root)/Multimediedesigner_cases/page.tsx
+++ app/(root)/Multimediedesigner_cases/page.tsx
@@ -3,14 +3,25 @@ import React from 'react'
 
 const page = async() => {
   const data = await FetchSelcet('Uddannelser','Uddannelse', 'Multimediedesigner')
-  console.log(data)
   return (
     <section className='container-col test mobiletest pb-[30px]'>
       <h1>Multimediedesigner cases</h1>
-      <p>Cases vist herunder blev alle lavet under min multimedie designer uddannelse på IBA</p>
-      {data.map((semester)=>(
-        <div key={semester.Title}>
-          <h2>{semester.Title}</h2>
+      <p className='mb-[2.5%]'>Cases vist herunder blev alle lavet under min multimedie designer uddannelse på IBA</p>
+      {data[0].Cases.map((semester:any)=>(
+        <div key={semester.Title} className='flex'>
+          <div className='p-[10px] w-[50%] flex flex-wrap content-start'>
+            <h1>{semester.Title}</h1>
+            <p>{semester.Text}</p>
+          </div>
+          <div>
+            <h1>Cases</h1>
+            {semester?.cases?.map((item:any)=>(
+              <p key={item}>
+                {item}
+              </p>
+            ))}
+          </div>
+
         </div>
       ))}
     </section>
